e9de5cb63a6d54c30dc078cf403470de9c874047
Chaurasia Rishav <aryan.rishav23@gmail.com>
2021-04-04 17:48:44 +0530

    android

---
+++ Google/app/src/main/java/com/example/google/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.google;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=(WebView)findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://ashish1323.github.io/google-clone/");
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else
+        super.onBackPressed();
+    }
+}
